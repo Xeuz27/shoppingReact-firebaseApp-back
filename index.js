1105583e64f61fe7b739303955fecd1107790a66
Jesus <jesusg.1301.jg@gmail.com>
2023-03-27 13:39:34 -0400

    adding access controll allow origin header to index,js

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 3005;
 // const CLIENT_PORT = process.env.CLIENT_PORT
 // const SECRET = process.env.SECRET
 
-
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   next();
+ });
+ 
 app.use(express.json())
 app.use(cors({
  origin: 'https://shopping-react-firebase-app-front.vercel.app',
